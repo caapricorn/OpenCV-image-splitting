163f28e8b7627846b68e2940cad9360402bd62ba
caapricorn <caapricorn@mail.ru>
2023-05-03 01:22:32 +0300

    autocommit-Ср 03 мая 2023 01:22:32 MSK

--- main.cpp
+++ main.cpp
@@ -80,9 +80,9 @@ int main( int argc, char** argv )
 	int ir = dst.rows;
 	int cr = dst.cols;
 	// Loop over the rows and columns
-    for (int currentRow = 0; currentRow <= dst.rows; currentRow += squareSize)
+    for (int currentRow = 0; currentRow <= ir; currentRow += squareSize)
     {
-        for (int currentCol = 0; currentCol <= dst.cols; currentCol += squareSize)
+        for (int currentCol = 0; currentCol <= cr; currentCol += squareSize)
         {
 			if (currentCol != 0) {
 				currentCol -= OFFSET_BETWEEN_SQURES;
