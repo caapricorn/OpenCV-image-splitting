f24766679fb309e6337b6f1a362844eb52f3761c
caapricorn <caapricorn@mail.ru>
2023-05-02 23:28:03 +0300

    autocommit-Вт 02 мая 2023 23:28:03 MSK

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgproc/imgproc_c.h>
 #include <opencv2/core/types_c.h>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
 #include <fstream>
 #include <iostream>
