1bcff266b6af83570d2b7a233fa43ea417e9c6d6
caapricorn <caapricorn@mail.ru>
2023-05-03 00:18:11 +0300

    autocommit-Ср 03 мая 2023 00:18:11 MSK

--- main.cpp
+++ main.cpp
@@ -83,7 +83,7 @@ int main( int argc, char** argv )
             cv::Mat square = dst(roi);
 
             // Do something with the square, for example display it
-			char name = "square" + i + j + ".bmp";
+			char name[] = "square" + i + j + ".bmp";
             imwrite(name, square);
             cv::waitKey(0);
         }
